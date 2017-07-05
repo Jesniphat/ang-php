9bd1f0b9c406af4197a1dc0ab36b94037f892f40
Jesse <jesniphat@hotmail.com>
2017-07-05 20:50:29 +0700

    change port 8010

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -9,7 +9,7 @@ export class ApiService {
     private prod:boolean = false; 
     public api:string = "http://127.0.0.1/project_shop_api/api.php";
     public upl:string = "http://127.0.0.1/project_shop_api/upload.php";
-    public img:string = "http://" + location.hostname + "/";
+    public img:string = "http://" + location.hostname + ":8010/";
     // public img:string = "http://192.168.99.100/";
 
   constructor(private http: Http) {
