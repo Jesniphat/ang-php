b1624e5e7c3aa9bb3eb2deff4af05f19ae5d3cd8
Jesniphat <jesniphat.p@firecreekweb.com>
2017-08-22 11:48:11 +0700

    Back to aws

--- server/library/config.js
+++ server/library/config.js
@@ -3,11 +3,11 @@ module.exports = new function() {
   this.init = function(){
     var mysql = require("mysql");
     db = mysql.createConnection({
-      host: "localhost",
+      host: "13.59.164.106",
       port: 3306,
       user: "root",
       password: "rootp@ssw0rd",
-      database: "project-jphp"
+      database: "project_jphp"
     });
     db.connect(function(err){
       if(err) {
